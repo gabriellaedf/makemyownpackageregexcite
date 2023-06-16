18e69d31850e76632177e3fe4a67a8398086f0a3
gabriellaedf <gabriella.edfeldt@ki.se>
2023-06-16 11:52:56 +0200

    updated name and function

---
+++ R/str_split_one.R
@@ -0,0 +1,21 @@
+#' Split a string
+#'
+#' @param string A character vector with one element
+#' @param pattern What to split
+#' @param n maximum number of pieces to return
+#'
+#' @return A character vector
+#' @export
+#'
+#' @examples
+#' (x<-"alfa,bravo,charlie,delta")
+#' str_split_one(x,pattern=",")
+#'
+str_split_one<-function(string, pattern, n=Inf){
+  stopifnot(is.character(string),length(string)<=1)
+  if(length(string)==1){
+    stringr::str_split(string=string, pattern=pattern,n=n)[[1]]
+  }else{
+    character()
+  }
+}
